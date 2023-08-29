c6165e1b482c9fbf65e8f08f34b37803238aad9e
DiegoCastro7 <137318162+DiegoCastro7@users.noreply.github.com>
2023-08-29 18:01:08 -0500

    feat :hammer: Files for large video playback

--- storage/PlayV.js
+++ storage/PlayV.js
@@ -1,18 +1,36 @@
-let localID=localStorage.getItem('id')
-let video = async()=>{
-    let seleccion = document.querySelector(".play-video");
-    let element =`
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/erKosEQaaFc?si=sCnVoyhTNlOgsw5h" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+const path4 = "storage/VideoI";
+ let localID=localStorage.getItem('id')
+ let video = async()=>{
+     let seleccion = document.querySelector(".play-video");
+     seleccion.insertAdjacentHTML("afterbegin",/*html*/`
+     <iframe width="100%" height="16.5%" src="https:www.youtube.com/embed/${localID}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+     `);
 
-    `
-    console.log(element)
-    seleccion.insertAdjacentHTML("beforeend",element);
-}
+     let peticion = await fetch(`${path4}.json`);
+     let res = await peticion.json();
+     let seleccion2 = document.querySelector(".publisher");
+     seleccion2.insertAdjacentHTML("afterbegin",/*html*/`
+     <img src=${res.author.avatar[2].url}>
+        <div>
+            <p>${res.author.title}</p>
+            <span>${res.author.stats.subscribersText}</span>
+        </div>
+        <button type="button">Subscribe</button>
+     `);
+     let res3 = await peticion.json();
+     let seleccion3 = document.querySelector(".vid-description");
+     seleccion3.insertAdjacentHTML("afterbegin",/*html*/`
+     <img src=${res.author.avatar[2].url}>
+     <p>Channel that makes learn easy</p>
+     <p>Subscribe Easy tutorials to watch more turotials on web development</p>
+     <hr>
+     <h4>${res.stats.comments} Comments</h4>
+     <div class="add-comment">
+         <img src="images/avatar.png">
+         <input type="text" placeholder="Write comments...">
+     </div>
+     `);
+ }
 video()
-// }
-// function ponerVideo(parametro) {
-//     let iframe = document.querySelector("#video-left");
-//     iframe.insertAdjacentHTML('afterbegin', /HTML/
-//     <iframe width="100%" height="730" src="https://www.youtube.com/embed/${parametro}?si=czx-JXcyfxDxe0lv&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-//     )
-// }
+
+
